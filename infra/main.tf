d1d0c1c6c8e0ac8aceb5c6156d32b2d5f4b7f2e5
Ambika <rudrambika.angadi@docc.techstarter.de>
2023-11-21 21:09:16 +0100

    seventh workflow bucket secret name created

--- infra/main.tf
+++ infra/main.tf
@@ -7,3 +7,11 @@ module "s3-website" {
   source = "./modules/s3-website"
   bucket_name = var.bucket_name
 }
+
+output "bucket_name" {
+  value = aws_s3_bucket.this.bucket
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+}

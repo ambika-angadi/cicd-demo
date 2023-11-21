c9e8e967ef27dfa882eaafbd1a678f248ae6ec32
Ambika <rudrambika.angadi@docc.techstarter.de>
2023-11-21 20:09:49 +0100

    sixth workflow bucket name not hardcoded

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,9 @@
+# module "s3-website" {
+#   source = "./modules/s3-website"
+#   bucket_name = "my-website-bucket-234808sfd2"
+# }
+
 module "s3-website" {
   source = "./modules/s3-website"
-  bucket_name = "my-website-bucket-234808sfd2"
+  bucket_name = var.bucket_name
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -6,3 +6,7 @@ variable "aws_profile" {
   type    = string
   default = "techstarter"
 }
+
+variable "bucket_name" {
+  type = string
+}
